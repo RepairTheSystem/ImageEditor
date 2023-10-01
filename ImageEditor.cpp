ae3dd0d64c1b029f236655bbdd560b14b6a9cfa6
Oleg <olegshutko54@gmail.com>
2023-10-01 17:00:56 +0300

    Added second rotation func

--- ImageEditor.cpp
+++ ImageEditor.cpp
@@ -31,8 +31,29 @@ struct pixel_struct{
     uint8_t blue;
 };
 
+void rotate_left(vector<pixel_struct>& pix, BMPHeader &header) {
+    vector<pixel_struct> new_pix(pix.size());
+    int width = header.width;
+    int height = header.height;
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int new_x = height - y - 1;
+            int new_y = x;
+
+            int original_index = y * width + x;
+            int new_index = new_y * height + new_x;
+
+            new_pix[new_index] = pix[original_index];
+        }
+    }
+    header.width = height;
+    header.height = width;
+    pix = new_pix;
+}
+
 // Функция для поворота изображения на 90 градусов по часовой стрелке
-void rotateImage(std::vector<pixel_struct>& pix, BMPHeader &header) {
+void rotate_right(vector<pixel_struct>& pix, BMPHeader &header) {
     vector<pixel_struct> new_pix(pix.size());
     int width = header.width;
     int height = header.height;
@@ -72,8 +93,6 @@ int main() {
     vector<pixel_struct> pix(width * height);
 
     img.read(reinterpret_cast<char*>(pix.data()), pix.size() * 3);
-    
-    rotateImage(pix, header);
 
     new_img.write(reinterpret_cast<char*>(&header), sizeof(header));
     new_img.write(reinterpret_cast<char*>(pix.data()), pix.size() * 3);
